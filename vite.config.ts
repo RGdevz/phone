4d8c3ab93b8d932aaaabf63a97972853715fea7e
RGdevz <72873414+RGdevz@users.noreply.github.com>
2025-04-05 19:19:36 +0300

    q

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 
-  base:'/phone',
+  base:'/phone/',
 
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
